06a91081e0436166556e19cd23969b98b1316202
Mert Karabulut <karabulutmert90@gmail.com>
2024-06-12 12:31:47 +0300

    diff files

--- swiftlint.sh
+++ swiftlint.sh
@@ -26,22 +26,29 @@
 # https://help.github.com/en/github/automating-your-workflow-with-github-actions/development-tools-for-github-actions#logging-commands
 
 function stripPWD() {
-    sed -E "s/$(pwd|sed 's/\//\\\//g')\///"
+  sed -E "s/$(pwd|sed 's/\//\\\//g')\///"
 }
 
 function convertToGitHubActionsLoggingCommands() {
-    sed -E 's/^(.*):([0-9]+):([0-9]+): (warning|error|[^:]+): (.*)/::\4 file=\1,line=\2,col=\3::\5/'
+  sed -E 's/^(.*):([0-9]+):([0-9]+): (warning|error|[^:]+): (.*)/::\4 file=\1,line=\2,col=\3::\5/'
 }
 
-if ! ${DIFF_BASE+false};
-then
-	changedFiles=$(git --no-pager diff --name-only --relative FETCH_HEAD $(git merge-base FETCH_HEAD $DIFF_BASE) -- '*.swift')
+if [ -n "$DIFF_BASE" ] && [ -n "$DIFF_HEAD" ]; then
+  changedFiles=$(git --no-pager diff --name-only --relative $DIFF_HEAD $DIFF_BASE -- '*.swift')
 
-	if [ -z "$changedFiles" ]
-	then
-		echo "No Swift file changed"
-		exit
-	fi
+  if [ -z "$changedFiles" ]
+  then
+    echo "No Swift file changed"
+    exit
+  fi
+elif [ -n "$DIFF_BASE" ]; then
+  changedFiles=$(git --no-pager diff --name-only --relative FETCH_HEAD $(git merge-base FETCH_HEAD $DIFF_BASE) -- '*.swift')
+
+  if [ -z "$changedFiles" ]
+  then
+    echo "No Swift file changed"
+    exit
+  fi
 fi
 
 set -o pipefail && swift run $SWIFTLINT_PACKAGE_ARGS --skip-build swiftlint "$@" -- $changedFiles | stripPWD | convertToGitHubActionsLoggingCommands
