999fccd9d71a81638a6d51a5ec19b11a6dd30944
Mert Karabulut <karabulutmert90@gmail.com>
2024-06-12 13:25:53 +0300

    Update swiftlint.sh

--- swiftlint.sh
+++ swiftlint.sh
@@ -33,22 +33,24 @@ function convertToGitHubActionsLoggingCommands() {
   sed -E 's/^(.*):([0-9]+):([0-9]+): (warning|error|[^:]+): (.*)/::\4 file=\1,line=\2,col=\3::\5/'
 }
 
-if [ -n "$DIFF_BASE" ] && [ -n "$DIFF_HEAD" ]; then
-  changedFiles=$(git --no-pager diff --name-only --relative $DIFF_HEAD $DIFF_BASE -- '*.swift')
+sh -c "git config --global --add safe.directory $PWD"
+if ! ${WORKING_DIRECTORY+false};
+then
+    cd ${WORKING_DIRECTORY}
+fi
 
-  if [ -z "$changedFiles" ]
-  then
-    echo "No Swift file changed"
-    exit
-  fi
-elif [ -n "$DIFF_BASE" ]; then
-  changedFiles=$(git --no-pager diff --name-only --relative FETCH_HEAD $(git merge-base FETCH_HEAD $DIFF_BASE) -- '*.swift')
+if ! ${DIFF_BASE+false};
+then
+    # Find all Swift files in the repository
+    git fetch --prune --no-tags origin "+refs/heads/${GITHUB_BASE_REF}:refs/heads/${GITHUB_BASE_REF}"
+    changedFiles=$(git diff --name-only $(git merge-base ${GITHUB_BASE_REF} ${GITHUB_HEAD_REF})..${GITHUB_HEAD_REF} | grep '\.swift$')
+    echo "changed files $changedFiles"
 
-  if [ -z "$changedFiles" ]
-  then
-    echo "No Swift file changed"
-    exit
-  fi
+    if [ -z "$changedFiles" ]
+    then
+        echo "No Swift file changed"
+        exit
+    fi
 fi
 
 set -o pipefail && swift run $SWIFTLINT_PACKAGE_ARGS --skip-build swiftlint "$@" -- $changedFiles | stripPWD | convertToGitHubActionsLoggingCommands
